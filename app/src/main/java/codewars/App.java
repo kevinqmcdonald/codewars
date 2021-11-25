281eca255f18a72f3d9031d07ee3d869774f20d8
kevinqmcdonald <kevinqmcdonald@gmail.com>
2021-11-24 23:24:33 -0500

    update default

--- app/src/main/java/codewars/App.java
+++ app/src/main/java/codewars/App.java
@@ -4,11 +4,7 @@
 package codewars;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        // main
     }
 }
