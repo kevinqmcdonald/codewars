a27053a0214fcdf1556f7286dd252b7b4b81d241
kevinqmcdonald <kevinqmcdonald@gmail.com>
2021-11-26 21:55:04 -0500

    solution

--- app/src/main/java/codewars/App.java
+++ app/src/main/java/codewars/App.java
@@ -4,7 +4,30 @@
 package codewars;
 
 public class App {
-    public static void main(String[] args) {
-        // main
+  static int find(int[] integers) {
+    // Check the optimal case
+    int first = integers[0];
+    int second = integers[1];
+    if(!isSameParity(first, second)) {
+      if(isSameParity(first, integers[2])) {
+        return second;
+      } else {
+        return first;
+      }
     }
+
+    // iterate until we find the different one
+    boolean isOdds = first % 2 == 1;
+    for(int i = 2; i < integers.length; ++i) {
+      if((isOdds && Math.abs(integers[i] % 2) == 0) || (!isOdds && Math.abs(integers[i] % 2) == 1)) {
+        return integers[i];
+      }
+    }
+
+    throw new IllegalArgumentException("array doesn't contain an outlier");
+  }
+
+  static boolean isSameParity(int i, int j) {
+    return Math.abs(i % 2) == Math.abs(j % 2);
+  }
 }
--- app/src/test/java/codewars/AppTest.java
+++ app/src/test/java/codewars/AppTest.java
@@ -3,6 +3,22 @@
  */
 package codewars;
 
-class AppTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+  @Test
+  public void testExample() {
+    int[] exampleTest1 = {2,6,8,-10,3};
+    int[] exampleTest2 = {206847684,1056521,7,17,1901,21104421,7,1,35521,1,7781};
+    int[] exampleTest3 = {Integer.MAX_VALUE, 0, 1};
+    int[] exampleTest4 = {2, 4, 0, 100, 4, 11, 2602, 36};
+    int[] exampleTest5 = {161, 3, 1719, 19, 11, 13, -20};
+    assertEquals(3, App.find(exampleTest1));
+    assertEquals(206847684, App.find(exampleTest2));
+    assertEquals(0, App.find(exampleTest3));
+    assertEquals(11, App.find(exampleTest4));
+    assertEquals(-20, App.find(exampleTest5));
+  }
 }
