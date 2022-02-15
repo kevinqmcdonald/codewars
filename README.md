f9b29b1c9af6f3b692ff6102e6b07c4220e28d82
kevinqmcdonald <kevinqmcdonald@gmail.com>
2022-02-15 17:49:35 -0500

    solution

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-# codewars
-Testing skeleton for [codewars](https://www.codewars.com/) Java Katas.
-- Base skeleton is `master`
-- Individual katas are branches
+# The Greatest Warrior - 4 Kyu
+[Link](https://www.codewars.com/kata/5941c545f5c394fef900000c)
--- app/src/test/java/codewars/AppTest.java
+++ app/src/test/java/codewars/AppTest.java
@@ -3,6 +3,38 @@
  */
 package codewars;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class AppTest {
 
+  @Test
+  void sampleTest1() {
+    Warrior tom = new Warrior();
+
+    assertEquals(1,          tom.level());
+    assertEquals(100,        tom.experience());
+    assertEquals("Pushover", tom.rank());
+  }
+
+  @Test
+  void sampleTest2() {
+    Warrior bruce_lee = new Warrior();
+
+    assertEquals(1,                                      bruce_lee.level());
+    assertEquals(100,                                    bruce_lee.experience());
+    assertEquals("Pushover",                             bruce_lee.rank());
+    assertEquals(new ArrayList<String>(),                         bruce_lee.achievements());
+    assertEquals("Defeated Chuck Norris",                bruce_lee.training("Defeated Chuck Norris", 9000, 1));
+    assertEquals(9100,                                   bruce_lee.experience());
+    assertEquals(91,                                     bruce_lee.level());
+    assertEquals("Master",                               bruce_lee.rank());
+    assertEquals("A good fight",                         bruce_lee.battle(90));
+    assertEquals(9105,                                   bruce_lee.experience());
+    assertEquals(Arrays.asList("Defeated Chuck Norris"),          bruce_lee.achievements());
+  }
 }
